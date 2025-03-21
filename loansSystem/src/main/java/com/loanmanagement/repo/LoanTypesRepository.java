548186792fe97e286a04ebe956cb77a11bfe0322
AngelAlbertoCT <angelcruz421@aragon.unam.mx>
2025-03-20 19:34:46 -0600

    Working again

--- loansSystem/src/main/java/com/loanmanagement/repo/LoanTypesRepository.java
+++ loansSystem/src/main/java/com/loanmanagement/repo/LoanTypesRepository.java
@@ -1,8 +1,11 @@
 package com.loanmanagement.repo;
 
+import com.loanmanagement.model.AccountTypeModel;
+import com.loanmanagement.model.LoanTypes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanTypesRepository extends JpaRepository<com.loanmanagement.model.LoanTypes, Long> {
+    LoanTypes findById(long id);
 }
